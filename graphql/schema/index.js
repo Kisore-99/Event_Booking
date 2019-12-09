a45aac7cc37bb5824159ad3554769dbed698f146
Kisore-99 <lakisore111@gmail.com>
2019-12-10 00:53:29 +0530

    Schema

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,6 +1,13 @@
 const {buildSchema } =require('graphql');
 
 module.exports= buildSchema (`
+type Booking{
+    _id: ID!
+    event: Event!
+    user: User!
+    createdAt: String!
+    updatedAt: String!
+}
 
 type Event{
     _id: ID!
@@ -9,6 +16,7 @@ type Event{
     price: Float!
     date: String!
     creator: User!
+    
 }
 type User {
     _id: ID!
@@ -30,11 +38,14 @@ input UserInput{
 
 type RootQuery {
     events: [Event!]!
+    bookings: [Booking!]!
 }
 
 type RootMutation{
     createEvent(eventInput: EventInput): Event
     createUser(userInput: UserInput): User
+    bookEvent(eventId: ID!): Booking!
+    cancelBooking(bookingId: ID!): Event!
 }
 schema{
     query: RootQuery
