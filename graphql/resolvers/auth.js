a16ae20541bca1c69caaa0fbe1edbae83ae275ac
Kisore-99 <lakisore111@gmail.com>
2019-12-10 11:50:07 +0530

    Authentication

---
+++ graphql/resolvers/auth.js
@@ -0,0 +1,32 @@
+const bcrypt= require('bcryptjs');
+const User= require('../../models/user');
+
+
+
+module.exports= {
+    createUser :async (args)=>{
+        try{
+        const existingUser= await User.findOne({
+            email: args.userInput.email
+        })
+            if(existingUser){
+                throw new Error('User exists already')
+            }
+        const hashedPassword= await bcrypt
+        .hash(args.userInput.password, 12)
+            const user= new User({
+                email: args.userInput.email,
+                password: hashedPassword
+            })
+            const result= await user.save();
+
+            return { ...result._doc, password: null,  _id: result.id } //to not return password, it is set to null
+        }
+
+        catch(err){
+            throw err;
+        }
+        
+    },
+
+}
