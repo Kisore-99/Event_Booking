42cc0d94d6a70fe7154f34630281b8a437edbeff
Kisore-99 <lakisore111@gmail.com>
2019-12-11 14:35:57 +0530

    Client UI

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import AuthPage from './pages/Auth';
 import BookingsPage from './pages/Bookings';
 import EventsPage from './pages/Events';
-//import MainNavigation from './components/Navigation/MainNavigation';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 import './App.css';
 
@@ -13,15 +13,15 @@ class App extends Component {
     return (
       <BrowserRouter>
         <React.Fragment>
-          
-          <main className="main-content">
-            <Switch>
-              <Redirect from="/" to="/auth" exact />
-              <Route path="/auth" component={AuthPage} />
-              <Route path="/events" component={EventsPage} />
-              <Route path="/bookings" component={BookingsPage} />
-            </Switch>
-          </main>
+          <MainNavigation />          
+              <main className="main-content">
+                <Switch>
+                  <Redirect from="/" to="/auth" exact />
+                  <Route path="/auth" component={AuthPage} />
+                  <Route path="/events" component={EventsPage} />
+                  <Route path="/bookings" component={BookingsPage} />
+                </Switch>
+              </main>
         </React.Fragment>
       </BrowserRouter>
     );
