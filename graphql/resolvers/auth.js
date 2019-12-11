bc6b8fae9a13999c4064b4bd09aa145f78a70ff3
Kisore-99 <lakisore111@gmail.com>
2019-12-11 14:39:07 +0530

    updated

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,6 +1,6 @@
 const bcrypt= require('bcryptjs');
 const User= require('../../models/user');
-
+const jwt= require('jsonwebtoken');
 
 
 module.exports= {
@@ -28,5 +28,22 @@ module.exports= {
         }
         
     },
-
+    login: async ({ email, password }) => {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+          throw new Error('User does not exist!');
+        }
+        const isEqual = await bcrypt.compare(password, user.password);
+        if (!isEqual) {
+          throw new Error('Password is incorrect!');
+        }
+        const token = jwt.sign(
+          { userId: user.id, email: user.email },
+          'somesupersecretkey',
+          {
+            expiresIn: '1h'
+          }
+        );
+        return { userId: user.id, token: token, tokenExpiration: 1 };
+      }
 }
