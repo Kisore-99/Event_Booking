42fb5ced887e46b4e0f10f2dffd447b6bf2f2ef5
Kisore-99 <lakisore111@gmail.com>
2019-12-12 23:48:58 +0530

    Resolvers Updated

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,64 +1,75 @@
-const Event= require('../../models/event');
-const User= require('../../models/user');
-const { dateToString }=require('../../helpers/data');
-const events= async eventIds=>{
-    try{
-    const events= await Event.find({_id : {$in: eventIds}})
-     return events.map(event=>{
-            return transformEvent(event);
+const DataLoader = require('dataloader');
+
+const Event = require('../../models/event');
+const User = require('../../models/user');
+
+const { dateToString } = require('../../helpers/data');
+
+//dataloader always needs an identifier
+const eventLoader = new DataLoader((eventIds) => {
+  return events(eventIds);
+});
+
+const userLoader = new DataLoader(userIds => {
+  return User.find({_id: {$in: userIds}});
+});
+
+const events = async eventIds => {
+  try {
+    const events = await Event.find({ _id: { $in: eventIds } });
+    return events.map(event => {
+      return transformEvent(event);
     });
-    
-  }catch(err){
-        throw err;
-    }
-}
+  } catch (err) {
+    throw err;
+  }
+};
 
-const singleEvent= async eventId =>{
-    try{
-        const event= await Event.findById(eventId);
-        return transformEvent(event);
+const singleEvent = async eventId => {
+  try {
+    const event = await eventLoader.load(eventId.toString()); //converting object into string as onjects with same values are treated as different things in JS
+    return event;
+  } catch (err) {
+    throw err;
+  }
+};
 
-    }catch(err){
-        throw err;
-    }
-}
-
-
-const user= userId =>{
-    return User.findById(userId)
-    .then(user =>{
-        return { ...user._doc,
-             _id: user.id, 
-             createdEvents: events.bind(this, user._doc.createdEvents) 
-        }
-    })
-    .catch(err =>{
-        throw err;
-    })
-} 
-
-const transformEvent= event =>{
+const user = async userId => {
+  try {
+    const user = await userLoader.load(userId.toString()); //converting object into string as onjects with same values are treated as different things in JS
     return {
-        ...event._doc,
-        _id: event.id,
-        date: dateToString(event._doc.date),
-        creator: user.bind(this, event.creator)// it indicates creator is not s single value, it calls a function when it is invoked
-    }
-}
+      ...user._doc,
+      _id: user.id,
+      createdEvents: () => eventLoader.loadMany(user._doc.createdEvents)
+    };
+  } catch (err) {
+    throw err;
+  }
+};
 
-const transformBooking= booking =>{
-    return {
-        ...booking._doc,
-        _id: booking.id , 
-        user: user.bind(this, booking._doc.user),
-        event: singleEvent.bind(this, booking._doc.event),
-        createdAt: dateToString(booking._doc.createdAt),
-        updatedAt: dateToString(booking._doc.updatedAt) 
-       }
-}
+const transformEvent = event => {
+  return {
+    ...event._doc,
+    _id: event.id,
+    date: dateToString(event._doc.date),
+    creator: user.bind(this, event.creator)
+  };
+};
 
-exports.transformEvent= transformEvent;
-exports.transformBooking= transformBooking;
-//exports.user= user;
-// exports.events= events;
-//exports.singleEvent= singleEvent;
+const transformBooking = booking => {
+  return {
+    ...booking._doc,
+    _id: booking.id,
+    user: user.bind(this, booking._doc.user),
+    event: singleEvent.bind(this, booking._doc.event),
+    createdAt: dateToString(booking._doc.createdAt),
+    updatedAt: dateToString(booking._doc.updatedAt)
+  };
+};
+
+exports.transformEvent = transformEvent;
+exports.transformBooking = transformBooking;
+
+// exports.user = user;
+// exports.events = events;
+// exports.singleEvent = singleEvent;
